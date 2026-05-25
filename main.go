71505696b7cb649d8f6ae0b85f7562525080da24
Pavel Denisov <padeniso@gmail.com>
2026-05-25 08:05:32 +0300

    separate functions for user input and conversion

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+func userData() (curType string, curAmount float64) {
+	fmt.Println("Введите исходный тип валюты (USD, EUR, RUB):")
+	fmt.Scan(&curType)
+	fmt.Println("Введите сумму:")
+	fmt.Scan(&curAmount)
+	return curType, curAmount
+}
+
+func convertCurrency(fromCur string, toCur string, curAmount float64) (conversionResult, float64) {
+
+}
+
 func main() {
 	const usdToEur = 0.85
 	const usdToRub = 75.0
