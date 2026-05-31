10f6d32c5551b5d18f2765037f759735d77002c4
Pavel Denisov <padeniso@gmail.com>
2026-05-31 09:27:01 +0300

    Doing calculations

--- main.go
+++ main.go
@@ -2,20 +2,67 @@ package main
 
 import "fmt"
 
-func userData() (curType string, curAmount float64) {
-	fmt.Println("Введите исходный тип валюты (USD, EUR, RUB):")
-	fmt.Scan(&curType)
-	fmt.Println("Введите сумму:")
-	fmt.Scan(&curAmount)
-	return curType, curAmount
+func userData() (fromCur string, toCur string,curAmount float64) {
+	for {
+		fmt.Println("Введите исходный тип валюты (USD, EUR, RUB):")
+		fmt.Scan(&fromCur)
+		if fromCur != "USD" && fromCur != "EUR" && fromCur != "RUB" {
+			fmt.Println("Неверный тип валюты. Пожалуйста, введите USD, EUR или RUB.")
+		} else {
+			break
+		}
+	}
+	for {
+		fmt.Println("Введите сумму:")
+		fmt.Scan(&curAmount)
+		if curAmount <= 0 {
+			fmt.Println("Неверная сумма. Пожалуйста, введите положительное число.")
+		} else {
+			break
+		}
+	}
+	for {		
+		fmt.Println("Введите целевой тип валюты (USD, EUR, RUB):")
+		fmt.Scan(&toCur)
+		if toCur != "USD" && toCur != "EUR" && toCur != "RUB" {
+			fmt.Println("Неверный тип валюты. Пожалуйста, введите USD, EUR или RUB.")}
+		else if toCur == fromCur {
+			fmt.Println("Целевой тип валюты должен отличаться от исходного. Пожалуйста, выберите другой тип.")
+		} else {
+			return fromCur, toCur, curAmount
+		}
+	}
 }
 
 func convertCurrency(fromCur string, toCur string, curAmount float64) (conversionResult float64) {
-	return 0.0
+	switch fromCur {
+	case "USD":
+		if toCur == "EUR" {
+			conversionResult = curAmount * usdToEur
+		} else if toCur == "RUB" {
+			conversionResult = curAmount * usdToRub
+		}
+	case "EUR":
+		if toCur == "USD" {
+			conversionResult = curAmount / usdToEur
+		} else if toCur == "RUB" {
+			conversionResult = curAmount / usdToEur * usdToRub
+		}
+	case "RUB":
+		if toCur == "USD" {
+			conversionResult = curAmount / usdToRub
+		} else if toCur == "EUR" {
+			conversionResult = curAmount / usdToRub * usdToEur
+		}
+	}
+	return conversionResult
 }
 
 func main() {
 	const usdToEur = 0.85
 	const usdToRub = 75.0
 	const eurToRub = usdToEur * usdToRub
+	fromCur, toCur, curAmount := userData()
+	result := convertCurrency(fromCur, toCur, curAmount)
+	fmt.Printf("%.2f %s = %.2f %s\n", curAmount, fromCur, result, toCur)
 }
