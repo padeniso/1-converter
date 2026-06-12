6e5330ea269043ca9134ad761de476818d4d26dc
Pavel Denisov <padeniso@gmail.com>
2026-06-12 22:32:03 +0300

    case changed to map

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func userData() (fromCur string, toCur string,curAmount float64) {
+func userData() (fromCur string, toCur string, curAmount float64) {
 	for {
 		fmt.Println("Введите исходный тип валюты (USD, EUR, RUB):")
 		fmt.Scan(&fromCur)
@@ -21,43 +21,48 @@ func userData() (fromCur string, toCur string,curAmount float64) {
 			break
 		}
 	}
-	for {		
+	for {
 		fmt.Println("Введите целевой тип валюты (USD, EUR, RUB):")
 		fmt.Scan(&toCur)
 		if toCur != "USD" && toCur != "EUR" && toCur != "RUB" {
-			fmt.Println("Неверный тип валюты. Пожалуйста, введите USD, EUR или RUB.")}
-		else if toCur == fromCur {
-			fmt.Println("Целевой тип валюты должен отличаться от исходного. Пожалуйста, выберите другой тип.")
-		} else {
-			return fromCur, toCur, curAmount
+			fmt.Println("Неверный тип валюты. Пожалуйста, введите USD, EUR или RUB.")
+			continue
 		}
+		if toCur == fromCur {
+			fmt.Println("Целевой тип валюты должен отличаться от исходного. Пожалуйста, выберите другой тип.")
+			continue
+		}
+		return fromCur, toCur, curAmount
 	}
+
 }
 
 func convertCurrency(fromCur string, toCur string, curAmount float64) (conversionResult float64) {
-	const usdToEur = 0.85
-	const usdToRub = 75.0
-	const eurToRub = usdToEur * usdToRub
-	switch fromCur {
-	case "USD":
-		if toCur == "EUR" {
-			conversionResult = curAmount * usdToEur
-		} else if toCur == "RUB" {
-			conversionResult = curAmount * usdToRub
-		}
-	case "EUR":
-		if toCur == "USD" {
-			conversionResult = curAmount / usdToEur
-		} else if toCur == "RUB" {
-			conversionResult = curAmount / usdToEur * usdToRub
-		}
-	case "RUB":
-		if toCur == "USD" {
-			conversionResult = curAmount / usdToRub
-		} else if toCur == "EUR" {
-			conversionResult = curAmount / usdToRub * usdToEur
-		}
-	}
+	myCurMap := map[string]float64{"USDEUR": 0.85, "USDRUB": 75.0, "EURUSD": 1 / 0.85, "EURRUB": 75.0 / 0.85, "RUBUSD": 1 / 75.0, "RUBEUR": 0.85 / 75.0}
+	conversionResult = curAmount * myCurMap[fromCur+toCur]
+	// const usdToEur = 0.85
+	// const usdToRub = 75.0
+	// const eurToRub = usdToEur * usdToRub
+	// switch fromCur {
+	// case "USD":
+	// 	if toCur == "EUR" {
+	// 		conversionResult = curAmount * usdToEur
+	// 	} else if toCur == "RUB" {
+	// 		conversionResult = curAmount * usdToRub
+	// 	}
+	// case "EUR":
+	// 	if toCur == "USD" {
+	// 		conversionResult = curAmount / usdToEur
+	// 	} else if toCur == "RUB" {
+	// 		conversionResult = curAmount / usdToEur * usdToRub
+	// 	}
+	// case "RUB":
+	// 	if toCur == "USD" {
+	// 		conversionResult = curAmount / usdToRub
+	// 	} else if toCur == "EUR" {
+	// 		conversionResult = curAmount / usdToRub * usdToEur
+	// 	}
+	// }
 	return conversionResult
 }
 
